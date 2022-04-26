09a2bf7308a4de35188b866f9315c7a0cbeb79b3
giorgio1966 <jehaylock@misena.edu.co>
2022-04-26 11:25:11 -0500

    Ajustes de videos
    Ajustes de video  e imagen en actividad

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -12,7 +12,7 @@
     p.mb-4(data-aos="zoom-right") A continuación, conocerá los temas que se tratarán en este componente formativo. ¡Muchos éxitos en su proceso de aprendizaje!    
     figure.mb-4
       .video(data-aos="flip-right")
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/GXTHrucLiac" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
     p.mb-4 En la siguiente infografía se presenta la información relacionada con los parámetros que pueden alterar la calidad del agua potable, factor de riesgo en los sistemas de tratamiento y distribución:
     h3.mb-4 Parámetros que contaminan el agua potable 
     p.mb-4 Existen amenazas que ponen en riesgo el suministro de agua potable en una población, las cuales deben ser identificadas:
